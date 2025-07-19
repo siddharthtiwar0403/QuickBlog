f443de31427f3dfef82a9dae5d283ad8c81ba1b2
Siddharth Tiwari <siddharthtiwari1350@gmail.com>
2025-07-20 01:47:50 +0530

    port fixed again

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ app.use('/api/blog', blogRouter)
 
 
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//     console.log('Server is running on port ' + PORT)
-// })
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
 
 export default app;
